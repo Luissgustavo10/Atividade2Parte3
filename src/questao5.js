f68fd10ef350e56387225dc184767ff985d6217a
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-14 23:50:05 -0300

    Update questao5.js

--- src/questao5.js
+++ src/questao5.js
@@ -1,1 +1,11 @@
-
+function parseQueryString(query) {
+    const params = query.split("&");
+    const result = {};
+    params.forEach(par => {
+        const [chave, valorBruto] = par.split("=");
+        const valor = decodeURIComponent(valorBruto);
+        const numero = Number(valor);
+        result[chave] = isNaN(numero) ? valor : numero;
+    });
+    return result;
+}
