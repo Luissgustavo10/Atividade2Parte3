533dd650d095c6075a6e0f0eb40798884875b249
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-14 23:44:46 -0300

    Update questao2.js

--- src/questao2.js
+++ src/questao2.js
@@ -1,1 +1,22 @@
-
+function analisarTexto(texto) {
+    const palavras = texto
+        .replace(/[.,!?]/g, "")       
+        .split(/\s+/)                
+        .filter(p => p.length > 0); 
+    const totalPalavras = palavras.length;
+    const mapaFreq = {};
+    for (const palavra of palavras) {
+        mapaFreq[palavra] = (mapaFreq[palavra] || 0) + 1;
+    }
+    const frequenciaPalavras = Object.keys(mapaFreq).map(p => ({
+        palavra: p,
+        frequencia: mapaFreq[p]
+    }));
+    const somaTamanhos = palavras.reduce((acc, p) => acc + p.length, 0);
+    const tamanhoMedioPalavras = (somaTamanhos / totalPalavras).toFixed(1);
+    return {
+        totalPalavras,
+        frequenciaPalavras,
+        tamanhoMedioPalavras: Number(tamanhoMedioPalavras)
+    };
+}
