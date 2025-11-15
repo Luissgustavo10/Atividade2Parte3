e43a53de7097a53dbf51ec32df5f295b06ca7bec
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-14 23:47:43 -0300

    Update questao3.js

--- src/questao3.js
+++ src/questao3.js
@@ -1,1 +1,21 @@
+function normalizarUsuarios(json) {
+    const usuarios = JSON.parse(json);
+    return usuarios.map(usuario => {
+        const nomeNormalizado = usuario.nome
+            .trim()
+            .split(/\s+/)
+            .map(p => p.charAt(0).toUpperCase() + p.slice(1).toLowerCase())
 
+        const emailNormalizado = usuario.email.toLowerCase();
+        const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailNormalizado);
+        const idade = Number(usuario.idade);
+        const maiorDeIdade = idade >= 18;
+        return {
+            nome: nomeNormalizado,
+            email: emailNormalizado,
+            emailValido,
+            idade,
+            maiorDeIdade
+        };
+    });
+}
